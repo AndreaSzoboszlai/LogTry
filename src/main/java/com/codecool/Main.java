9e7e46165a60256f0753801cf55f2719c125e641
AndreaSzoboszlai <szobandi@gmail.com>
2019-05-21 12:27:29 +0200

    done

---
+++ src/main/java/com/codecool/Main.java
@@ -0,0 +1,13 @@
+package com.codecool;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(Main.class);
+        logger.info("Hello World");
+    }
+}
